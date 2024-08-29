31a1d5968fc485e56c62f8f852bba7db7b28d01a
mkayf <muhammadkaif150@gmail.com>
2024-08-30 00:51:14 +0500

    navbar creation in process

---
+++ partials/navbar.php
@@ -0,0 +1,31 @@
+<header>
+    <nav>
+        <div class="nav-links-div">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Products
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Gift Articles</a></li>
+                    <li><a class="dropdown-item" href="#">Greeting Cards</a></li>
+                    <li><a class="dropdown-item" href="#">Files</a></li>
+                    <li><a class="dropdown-item" href="#">Dolls</a></li>
+                    <li><a class="dropdown-item" href="#">HandBags</a></li>
+                    <li><a class="dropdown-item" href="#">Wallets</a></li>
+                    <li><a class="dropdown-item" href="#">Beauty Products</a></li>
+                </ul>
+                </li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="nav-logo-div">
+            <h4>ARTS</h4>
+        </div>
+        <div class="nav-icons-div">
+            
+        </div>
+    </nav>
+</header>
