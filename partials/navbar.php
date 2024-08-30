5a3272a3b10d384f5e5ea3b48cce5803b9af7a73
mkayf <muhammadkaif150@gmail.com>
2024-08-31 00:09:35 +0500

    navbar created

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,31 +1,43 @@
 <header>
-    <nav>
+    <nav id="navbar">
         <div class="nav-links-div">
-            <ul>
+            <button class="collapse-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                <img src="/arts-shopping-cart/assets/images/menu-icon.png" alt="">
+            </button>
+            <ul class="mb-0 nav-link-ul">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Products
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Gift Articles</a></li>
-                    <li><a class="dropdown-item" href="#">Greeting Cards</a></li>
-                    <li><a class="dropdown-item" href="#">Files</a></li>
-                    <li><a class="dropdown-item" href="#">Dolls</a></li>
-                    <li><a class="dropdown-item" href="#">HandBags</a></li>
-                    <li><a class="dropdown-item" href="#">Wallets</a></li>
-                    <li><a class="dropdown-item" href="#">Beauty Products</a></li>
-                </ul>
-                </li>
+                <li><a href="#">Products</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
         </div>
         <div class="nav-logo-div">
-            <h4>ARTS</h4>
+            <h4 class="mb-0">ARTS</h4>
         </div>
         <div class="nav-icons-div">
-            
+            <!-- Search icon -->
+            <img src="/arts-shopping-cart/assets/images/search-btn.png" alt="search icon">
+            <!-- user icon -->
+            <img src="/arts-shopping-cart/assets/images/user-btn.png" alt="user icon">
+            <!-- cart-item-icon -->
+            <img src="/arts-shopping-cart/assets/images/cart-item-btn.png" alt="cart items icon">
         </div>
     </nav>
+    <!-- Offcanvas for responsive screens -->
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <!-- <h5 class="offcanvas-title" id="offcanvasExampleLabel">ARTS</h5> -->
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body d-flex flex-column justify-content-center align-items-center">
+    <div>
+      <ul class="offcanvas-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
 </header>
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-console.log(1);
+document.addEventListener('DOMContentLoaded', () => {
+    let navAnimate = false;
+
+    const changeValueOnScroll = () => {
+        const scrollValue = document?.documentElement?.scrollTop;
+        scrollValue > 100 ? navAnimate = true : navAnimate = false;
+        if(navAnimate){
+            document.getElementById('navbar').classList.add('sticky');
+        }
+        else{
+            document.getElementById('navbar').classList.remove('sticky');
+        }
+    }
+
+    window.addEventListener('scroll', changeValueOnScroll);
+
+})
