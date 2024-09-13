8d42269a47a2fd1c074d7e682cf2896a358b4f00
Abdul Samad <sammadaltaf43@gmail.com>
2024-09-13 18:43:08 +0500

    login form backend is completed

---
+++ DB.php
@@ -0,0 +1,25 @@
+
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $Database = "Arts-Db";
+
+    $connection = mysqli_connect($servername,$username,$password,$Database);
+    // if($connection){
+    //     ?>
+    //     <script>
+    //         alert("Database connection is sucessfull");
+    //     </script>
+    //     <?php
+    // }
+    // else{
+    //     ?>
+    //     <script>
+    //         alert("Database connection is erorr");
+    //     </script>
+    //     <?php
+    // }
+
+?>
